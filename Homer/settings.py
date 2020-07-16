1d148cce1a1a0e9968ac8f9ae3a5331d6586cb0f
Amr-Dawood <68282190+Amr-Dawood@users.noreply.github.com>
2020-07-16 14:39:03 +0200

    Vscode settings

--- Homer/settings.py
+++ Homer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pmg6f=r3_t#+wr0j4s#ny+(5=@=t1bqkh^viain_nl0ye%c8f3'
+SECRET_KEY = 'm#s$f3qvb5=arfo6s^-*=xl_2ij=%xxtlgwa!#srcotye3%&&w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
