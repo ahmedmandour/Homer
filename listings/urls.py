662c49e38e358467c45e7a68651c01fb23708664
Amr-Dawood <68282190+Amr-Dawood@users.noreply.github.com>
2020-07-20 12:09:04 +0200

    Revert "Search dispaly and filters"
    This reverts commit 8367853129550617d4ace3bca7c8e4f5a2f57ef0.

--- listings/urls.py
+++ listings/urls.py
@@ -6,8 +6,7 @@ urlpatterns = [
     
     path('', views.index, name='listings'),
     path('<int:listing_id>', views.listing, name='listing'),
-    path('search', views.search, name='search'),
+    path('search', views.search, name='listing'),
 
 ]
 
- 
