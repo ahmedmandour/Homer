68b6f46392097b9087d303dbbb74dc86359f3e59
Amr-Dawood <68282190+Amr-Dawood@users.noreply.github.com>
2020-07-20 02:43:51 +0200

    Display lists

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from djanfo.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from.models import Listing
+
 
 
 def index(request):
-    return render(request, 'listings/listings.html')
+
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings' : paged_listings
+    }
+
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
-    return render(request, 'listings/listings.html')
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk = listing_id)
+
+    context = {
+        'listing' : listing
+    }
+
+    return render(request, 'listings/listings.html', context)
 
 
 def search(request):
