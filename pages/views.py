84919c460fce305e6c2dfe2ea0ae39c81acdff6f
Amr-Dawood <68282190+Amr-Dawood@users.noreply.github.com>
2020-07-20 12:25:11 +0200

    Revert "Display lists"
    This reverts commit 68b6f46392097b9087d303dbbb74dc86359f3e59.

--- pages/views.py
+++ pages/views.py
@@ -1,31 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from listings.models import Listing
-from realtors.models import Realtor
-
 def index(request):
-    listings = Listing.objects.order_by(-list_date).filter(is_published=True)[:3]
-
-    context = {
-        'listings': Listings
-    }
-
-    return render(request, 'pages/index.html', context)
+    return render(request, 'pages/index.html')
 
 
 
 
 def about(request):
-    #GEt ALL REALTORS
-    realtors = Realtors.objects.order_by('-hire_date')
-
-    #  GET MVP
-    mvp_realtors =  Realtors.objects.all().filter(is_mvp = True)
-
-    context = {
-        'realtors' : realtors,
-        'mvp_realtors' : mvp_realtors
-    }
-
     return render(request, 'pages/about.html')
